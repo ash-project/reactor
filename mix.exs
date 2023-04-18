6c13ccc689269ce569dfbdab1b6f88783452e36b
Zach Daniel <zach@zachdaniel.dev>
2023-04-18 18:30:26 -0400

    chore: get ready for publish

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule Reactor.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      description: "A graph execution engine",
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        GitHub: "https://github.com/ash-project/reactor"
+      }
     ]
   end
 
@@ -23,6 +34,7 @@ defmodule Reactor.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
