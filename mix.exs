63a7cacd040c930d2a3fa9e6ba8ce72695914fff
James Harton <james@harton.nz>
2023-04-19 14:13:10 +1200

    chore: Add CI and dev/test deps.

--- mix.exs
+++ mix.exs
@@ -1,20 +1,30 @@
 defmodule Reactor.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "An asynchronous, graph-based execution engine"
+
   def project do
     [
       app: :reactor,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: @version,
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      description: "A graph execution engine",
+      deps: deps(),
       package: package(),
-      deps: deps()
+      description: @description,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases(),
+      source_url: "https://github.com/ash-project/reactor",
+      homepage_url: "https://github.com/ash-project/reactor",
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
   defp package do
     [
+      name: :reactor,
+      files: ~w[lib .formatter.exs mix.exs README* LICENSE* CHANGELOG* documentation],
       licenses: ["MIT"],
       links: %{
         GitHub: "https://github.com/ash-project/reactor"
@@ -32,9 +42,27 @@ defmodule Reactor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      # Dev/Test dependencies
+      {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false},
+      {:ex_check, "~> 0.12.0", only: [:dev, :test]},
+      {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false},
+      {:git_ops, "~> 2.4.4", only: [:dev, :test]},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp elixirc_paths(:test) do
+    elixirc_paths(:prod) ++ ["test/support"]
+  end
+
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp aliases do
+    [
+      sobelow: "sobelow --skip",
+      credo: "credo --strict"
     ]
   end
 end
