9220ba3da7e8dc6acb365a068cbae3effa3346a0
James Harton <james@harton.nz>
2023-09-28 09:07:53 +1300

    chore: add `mix spark.cheat_sheets` to `mix check`.

--- .check.exs
+++ .check.exs
@@ -11,11 +11,12 @@
     ## ...or adjusted (e.g. use one-line formatter for more compact credo output)
     # {:credo, "mix credo --format oneline"},
 
-    {:check_formatter, command: "mix spark.formatter --check"}
-
     ## custom new tools may be added (mix tasks or arbitrary commands)
     # {:my_mix_task, command: "mix release", env: %{"MIX_ENV" => "prod"}},
     # {:my_arbitrary_tool, command: "npm test", cd: "assets"},
     # {:my_arbitrary_script, command: ["my_script", "argument with spaces"], cd: "scripts"}
+
+    {:spark_formatter, "mix spark.formatter --check"},
+    {:spark_cheat_sheets, "mix spark.cheat_sheets --check"}
   ]
 ]
