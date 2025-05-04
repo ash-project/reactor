a2157ff49037e7bb6f1a6ed4604cf3eb4d398b7c
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2025-05-04 22:48:37 +0200

    chore: migrate from deprecated `igniter/2` to `igniter/1` (#211)
    Can't derive from commit history whether this constitutes a `chore` or `improvement` in this repo's book.

--- lib/mix/tasks/reactor.install.ex
+++ lib/mix/tasks/reactor.install.ex
@@ -11,7 +11,7 @@ if Code.ensure_loaded?(Igniter) do
 
     @doc false
     @impl Task
-    def igniter(igniter, _argv) do
+    def igniter(igniter) do
       igniter
       |> Formatter.import_dep(:reactor)
     end
