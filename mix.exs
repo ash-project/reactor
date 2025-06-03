4d6b08c9f5a457f4609d6c0de6533ffc3da480b3
James Harton <james@harton.nz>
2025-06-04 09:05:25 +1200

    chore(deps-dev): Remove `mix test_watch` because I don't use it.
    And I'm pretty sure I'm the only one who ever works on this thing TBH :)

--- mix.exs
+++ mix.exs
@@ -134,7 +134,6 @@ defmodule Reactor.MixProject do
       {:git_ops, "~> 2.7.0", only: [:dev, :test]},
       {:mimic, "~> 1.7", only: :test},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:sobelow, ">= 0.0.0", only: [:dev, :test], runtime: false}
     ]
   end
