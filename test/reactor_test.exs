b6747c985e9c755e43351da980d7e3f6c30dfd16
James Harton <james@harton.nz>
2025-09-13 01:14:44 +1200

    feat: Allow Reactors to be able to be undone after successful completion.

--- test/reactor_test.exs
+++ test/reactor_test.exs
@@ -52,5 +52,59 @@ defmodule ReactorTest do
 
       assert {:ok, "McFly Marty"} = Reactor.run(reactor, name: "Marty McFly")
     end
+
+    test "it can return successful reactors" do
+      assert {:ok, "Marty McFly", reactor} =
+               Reactor.run(BasicReactor, %{name: "McFly Marty"}, %{}, fully_reversible?: true)
+
+      assert reactor.state == :successful
+    end
+  end
+
+  describe "undo/2" do
+    defmodule UndoableReactor do
+      use Reactor
+
+      input :agent
+
+      step :push_a do
+        argument :agent, input(:agent)
+        run &push(&1, :a)
+        undo &undo/2
+      end
+
+      step :push_b do
+        wait_for :push_a
+        argument :agent, input(:agent)
+        run &push(&1, :b)
+        undo &undo/2
+      end
+
+      return :push_b
+
+      def push(args, value) do
+        Agent.update(args.agent, fn list -> [value | list] end)
+        {:ok, value}
+      end
+
+      def undo(value, args) do
+        Agent.update(args.agent, fn list -> List.delete(list, value) end)
+
+        :ok
+      end
+    end
+
+    test "previously successful reactors can be undone" do
+      {:ok, pid} = Agent.start_link(fn -> [:z] end)
+
+      assert {:ok, :b, reactor} =
+               Reactor.run(UndoableReactor, %{agent: pid}, %{}, fully_reversible?: true)
+
+      assert [:b, :a, :z] = Agent.get(pid, &Function.identity/1)
+
+      assert :ok = Reactor.undo(reactor, %{})
+
+      assert [:z] = Agent.get(pid, &Function.identity/1)
+    end
   end
 end
