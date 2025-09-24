4c61d308c1d96e539b8bcfc7d5ea825e6bb20ca8
James Harton <james@harton.nz>
2025-09-24 14:36:47 +1200

    feat: Support `backoff` in step DSL

--- lib/reactor/dsl/step.ex
+++ lib/reactor/dsl/step.ex
@@ -8,6 +8,7 @@ defmodule Reactor.Dsl.Step do
   defstruct __identifier__: nil,
             arguments: [],
             async?: true,
+            backoff: nil,
             compensate: nil,
             description: nil,
             guards: [],
@@ -23,6 +24,7 @@ defmodule Reactor.Dsl.Step do
   @type t :: %__MODULE__{
           arguments: [Dsl.Argument.t()],
           async?: boolean,
+          backoff: nil | (any, Reactor.inputs(), Reactor.context() -> :now | pos_integer()),
           compensate:
             nil | (any, Reactor.inputs(), Reactor.context() -> :ok | :retry | {:continue, any}),
           description: nil | String.t(),
@@ -111,14 +113,21 @@ defmodule Reactor.Dsl.Step do
           type: {:or, [{:mfa_or_fun, 1}, {:mfa_or_fun, 2}, {:mfa_or_fun, 3}]},
           required: false,
           doc: """
-          Provide an anonymous function which implements a `undo/1-3` callback. Cannot be provided at the same time as the `impl` argument.
+          Provide an anonymous function which implements a `undo/1..3` callback. Cannot be provided at the same time as the `impl` argument.
           """
         ],
         compensate: [
           type: {:or, [{:mfa_or_fun, 1}, {:mfa_or_fun, 2}, {:mfa_or_fun, 3}]},
           required: false,
           doc: """
-          Provide an anonymous function which implements a `compensate/1-3` callback. Cannot be provided at the same time as the `impl` argument.
+          Provide an anonymous function which implements a `compensate/1..3` callback. Cannot be provided at the same time as the `impl` argument.
+          """
+        ],
+        backoff: [
+          type: {:or, [{:mfa_or_fun, 1}, {:mfa_or_fun, 2}, {:mfa_or_fun, 3}]},
+          required: false,
+          doc: """
+          Provide an anonymous function which implements a `backoff/1..3` callback. Cannot be provided at the same time as the `impl` argument.
           """
         ],
         max_retries: [
@@ -203,9 +212,18 @@ defmodule Reactor.Dsl.Step do
            message: "Step has both an implementation module and a undo function"
          )}
 
+    defp rewrite_step(step, module) when not is_nil(step.impl) and not is_nil(step.backoff),
+      do:
+        {:error,
+         DslError.exception(
+           module: module,
+           path: [:reactor, :step, step.name],
+           message: "Step has both an implementation module and a backoff function"
+         )}
+
     defp rewrite_step(step, _dsl_state)
          when is_nil(step.run) and is_nil(step.compensate) and is_nil(step.undo) and
-                not is_nil(step.impl),
+                is_nil(step.backoff) and not is_nil(step.impl),
          do: {:ok, step}
 
     defp rewrite_step(step, _dsl_state),
@@ -214,10 +232,12 @@ defmodule Reactor.Dsl.Step do
          %{
            step
            | impl:
-               {Reactor.Step.AnonFn, run: step.run, compensate: step.compensate, undo: step.undo},
+               {Reactor.Step.AnonFn,
+                run: step.run, compensate: step.compensate, undo: step.undo, backoff: step.backoff},
              run: nil,
              compensate: nil,
-             undo: nil
+             undo: nil,
+             backoff: nil
          }}
   end
 end
--- lib/reactor/step/anon_fn.ex
+++ lib/reactor/step/anon_fn.ex
@@ -10,6 +10,8 @@ defmodule Reactor.Step.AnonFn do
     the compensate function of the step.  Optional.
   * `undo` - a one to three arity function or MFA which will be called as the
     undo function of this step.  Optional.
+  * `backoff` - a one to three arity function or MFA which will be called as
+    the backoff function of the step. Optional.
   """
 
   use Reactor.Step
@@ -75,6 +77,28 @@ defmodule Reactor.Step.AnonFn do
 
       _ ->
         :ok
+    end
+  end
+
+  @doc false
+  @impl true
+  @spec backoff(any, Reactor.inputs(), Reactor.context(), keyword) :: :now | pos_integer()
+  def backoff(reason, arguments, context, options) do
+    case Keyword.fetch(options, :backoff) do
+      {:ok, fun} when is_function(fun, 1) ->
+        fun.(reason)
+
+      {:ok, fun} when is_function(fun, 2) ->
+        fun.(reason, arguments)
+
+      {:ok, fun} when is_function(fun, 3) ->
+        fun.(reason, arguments, context)
+
+      {:ok, {m, f, a}} when is_atom(m) and is_atom(f) and is_list(a) ->
+        apply(m, f, [reason, arguments, context] ++ a)
+
+      _ ->
+        :now
     end
   end
 
